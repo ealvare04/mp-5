594acba19272eae8e7d78db61f6981cd9c10f909
Edison Alvarez <ealvare@bu.edu>
2026-04-10 23:46:50 -0400

    minor changes

--- components/UrlPreview.tsx
+++ components/UrlPreview.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
 
 export default function UrlPreview({url}: { url: UrlProps}) {
+    // hardcoded to vercel
+    const fullUrl = `https://mp-5-sigma-swart.vercel.app/r/${url.alias}`;
     return (
         <>
-            <Typography variant="body2" sx={{textAlign: "center", color: "gray"}}>
-                {`${window.location.origin}/r/${url.alias}`}
-            </Typography>
+            <Card sx={{width: "50%", m: 1, borderRadius: 3, backgroundColor: "#9dd6df", textAlign: "center", mx: "auto"}}>
+                <Typography variant="body2" sx={{textAlign: "center", color: "black"}}>
+                    {fullUrl}
+                </Typography>
+            </Card>
             <Card sx={{width: "50%", m: 1, borderRadius: 3, backgroundColor: "#38bdf8", textAlign: "center", mx: "auto"}}>
                 <CardActionArea component={Link} href={`/r/${url.alias}`}>
                     <CardContent>
-                        <Typography variant="h6">{url.alias}</Typography>
+                        <Typography variant="h6" sx={{color: "black"}}>{url.alias}</Typography>
                     </CardContent>
                 </CardActionArea>
             </Card>
